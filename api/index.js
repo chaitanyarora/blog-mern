8694d409d6dde3b1d2e56433df65cf800742df07
chaitanyarora <arorachaitany15@gmail.com>
2024-05-17 09:28:22 +0530

    After Auth

--- api/index.js
+++ api/index.js
@@ -4,39 +4,56 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 
-const salt = bcrypt.genSaltSync(10);;
+const salt = bcrypt.genSaltSync(10);
+const secret = 'djknfjkbjwfbfjwbejfbefw';
 
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect("mongodb+srv://chaitany:8pRl8FEZOHSlF1XL@cluster0.t9elit5.mongodb.net/");
 
 app.post('/register', async (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
     try {
         const userDoc = await User.create({
             username,
             password: bcrypt.hashSync(password, salt),
         })
-    res.json(userDoc);
+        res.json(userDoc);
 
-    } catch(e) {
+    } catch (e) {
         res.status(400).json(e);
     }
 });
 
 app.post('/login', async (req, res) => {
-    const {username, password} = req.body;
-    try {
-        const userDoc = await User.findOne({username});
-        
-        res.json(userDoc);
-    } catch(e) {
-        res.status(400).json(e);
-    }
- });
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
+    const passOk = bcrypt.compareSync(password, userDoc.password);
 
+    if(passOk) {
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+            if(err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+
+    } else {
+        res.status(400).json({message: 'Invalid username or password'});
+    }
+    
+});
+
+app.get('/profile', (req, res) => {
+    const token = req.cookies;
+    jwt.verify(token, secret, {},(err, info) => {
+        if(err) throw err;
+        res.json(info);
+    });
+});
 
 app.listen(4000);
--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -1,17 +1,30 @@
 import React, {useState} from 'react'
+import { Navigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
 
   async function login(ev) {
     ev.preventDefault();
     
-    await fetch('http://localhost:4000/login', {
+    const response = await fetch('http://localhost:4000/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({username, password})
-    })
+      body: JSON.stringify({username, password}),
+      credentials: 'include',
+    });
+
+    if(response.ok) {
+      setRedirect(true);
+    } else {
+      alert('Wrong credentials');
+    }
+  }
+
+  if(redirect) {
+    return <Navigate to='/' />
   }
 
   return (
