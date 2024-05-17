8e6a8caad8efe62df2dfdc52977665d950f32cc4
Chaitany Arora <arorachaitany15@gmail.com>
2024-05-17 12:09:38 +0530

    OfficePc: before create post

--- api/index.js
+++ api/index.js
@@ -3,15 +3,14 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
-const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 
-
+const app = express();
 const salt = bcrypt.genSaltSync(10);
 const secret = 'djknfjkbjwfbfjwbejfbefw';
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -23,9 +22,8 @@ app.post('/register', async (req, res) => {
         const userDoc = await User.create({
             username,
             password: bcrypt.hashSync(password, salt),
-        })
+        });
         res.json(userDoc);
-
     } catch (e) {
         res.status(400).json(e);
     }
@@ -36,24 +34,37 @@ app.post('/login', async (req, res) => {
     const userDoc = await User.findOne({ username });
     const passOk = bcrypt.compareSync(password, userDoc.password);
 
-    if(passOk) {
-        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
-            if(err) throw err;
-            res.cookie('token', token).json('ok');
+    if (passOk) {
+        jwt.sign({ username, id: userDoc._id.toString() }, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+                id: userDoc._id,
+                username,
+            });
         });
-
     } else {
-        res.status(400).json({message: 'Invalid username or password'});
+        res.status(400).json({ message: 'Invalid username or password' });
     }
-    
 });
 
 app.get('/profile', (req, res) => {
-    const token = req.cookies;
-    jwt.verify(token, secret, {},(err, info) => {
-        if(err) throw err;
+    const { token } = req.cookies; // Extract the token from cookies
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) {
+            return res.status(401).json({ message: 'Token is invalid' });
+        }
         res.json(info);
     });
 });
 
-app.listen(4000);
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,19 @@ import IndexPage from './Pages/IndexPage';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
 import RegisterPage from './Pages/RegisterPage';
+import {UserContextProvider} from './UserContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
