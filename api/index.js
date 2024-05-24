d8bdfaa214ff27cc45b215d6e22df03cc4ddc576
chaitanyarora <arorachaitany15@gmail.com>
2024-05-24 19:56:48 +0530

    Deleted Photos

--- api/index.js
+++ api/index.js
@@ -6,8 +6,6 @@ const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 
 const app = express();
-
-
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 
